81bc914a67edd69e185e3c15f2dee3535258cef7
Luke Fender <lfender6445@gmail.com>
2015-03-30 08:54:51 -0400

    updates

---
+++ vim/setup.sh
@@ -0,0 +1,3 @@
+  echo 'to setup your vim config, downloading plugged'
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
